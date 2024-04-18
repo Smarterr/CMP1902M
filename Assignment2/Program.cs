7b6bbb783b2f415afa20f630d605bf9d43db6fc1
Connor Adams <60903197+Smarterr@users.noreply.github.com>
2024-04-18 12:26:20 +0100

    Added Die.cs and Game.cs

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Create a game object and allow the user to pick which game they would like to play
+             */
+
+            Game game = new Game();
+            game.StartGame();
+        }
+    }
+}
