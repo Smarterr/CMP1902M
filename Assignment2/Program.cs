fe2c9ca4410dba8d1e627f1669482c916588eb75
Connor Adams <60903197+Smarterr@users.noreply.github.com>
2024-04-30 13:15:06 +0100

    Update Program.cs

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,18 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Assignment2
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            /*
-             * Create a game object and allow the user to pick which game they would like to play
-             */
-        }
-    }
-}
